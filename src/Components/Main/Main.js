2887cc36e7d4d1b8d30d2a51b8fff26355d61c79
Mayankkatheriya <mayankkatheriya4@gmail.com>
2023-12-16 12:00:26 +0530

    testimonyb started

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,12 +1,14 @@
 import './Main.css';
 import Hero from './Hero';
 import Summary from './Summary';
+import Testimonials from './Testimonials';
 
 function Main () {
     return (
         <main className="main">
             <Hero />
             <Summary />
+            <Testimonials />
         </main>
     )
 }
---
+++ src/Components/Main/Testimonials.js
@@ -0,0 +1,39 @@
+import './Testimonials.css'
+import image from '../Assets/testimony.avif'
+
+let textData = [
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime minima dicta amet, molestiae aliquam incidunt suscipit recusandae labore ratione doloremque, architecto et illo minus quo tenetur ducimus, voluptatibus repellendus fuga aperiam vel ab! Ipsam corrupti blanditiis dolorum! Officia assumenda rem nam, eveniet enim ad inventore laudantium est illum voluptatem quis.",
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolore vel quo deserunt quos expedita minima incidunt sed tempora, a architecto consectetur reprehenderit, in repellat voluptatum.",
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio beatae incidunt perferendis soluta facilis voluptas dicta repudiandae quasi asperiores libero, exercitationem molestiae autem sapiente dolore nulla non consequatur. Eaque, dolores.",
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus doloribus eius aut unde, dolores accusantium!",
+    "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi a voluptatum quidem nulla quisquam natus velit provident earum esse, odio numquam labore recusandae similique sunt.",
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius ut necessitatibus, repudiandae qui dolor minima."
+]
+
+const Testimonials = () => {
+    return (
+        <section className="testimonial">
+            <div className="testimonial-container">
+                {
+                    textData.map((data) => {
+                        return(
+                            <div className="testimony">
+                                <blockquote>{data}</blockquote>
+                                <div className="user-data">
+                                    <img src="{image}" alt="" />
+                                    <div className="user-dels">
+                                        <p>Gladis Lennon</p>
+                                        <p>Head of SEO</p>
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+            
+        </section>
+    )
+}
+
+export default Testimonials
